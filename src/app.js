1721a017865edc9d65a55b98d383ecd9500cb940
Enrique Montemayor <eemontemayor@live.com>
2019-08-15 10:20:03 -0500

    x

--- src/app.js
+++ src/app.js
@@ -23,7 +23,6 @@ app.use(cors())
 console.log('aqui')
   const text = req.query.text
   unirest.get( "https://translated-mymemory---translation-memory.p.rapidapi.com/api/get")
-  
   .query({
     "mt": "1",
     "onlyprivate": "0",
@@ -31,23 +30,16 @@ console.log('aqui')
     "langpair": "en|es",
     "q": text
   })
-  
   .headers({
     "x-rapidapi-host": "translated-mymemory---translation-memory.p.rapidapi.com",
     "x-rapidapi-key": "b5f70bd1c9msh133a4bbdfb3c433p1463c0jsn8429fb0c932c"
   })
-  
-  
   .end((response)=>{
-
-   
     if (res.error) throw new Error(res.error);
     console.log(res.body)
     return res.status(200).send(response)
-  
   })
-  
-     })
+})
 
 
 
